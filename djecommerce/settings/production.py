1e1cf6721d962f65ae8f299add8d0d7ab114cc51
makangachristopher <70667746+makangachristopher@users.noreply.github.com>
2024-04-09 01:06:08 +0300

    Update production.py

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = config('DEBUG', cast=bool)
-ALLOWED_HOSTS = ['ip-address', 'www.your-website.com']
+ALLOWED_HOSTS = ['ip-address', 'https://django-ecommerce-shop-1.onrender.com']
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
