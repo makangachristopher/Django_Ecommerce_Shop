66f533683126b76e3fd2475c141e25b772358a7c
makangachristopher <70667746+makangachristopher@users.noreply.github.com>
2024-04-09 08:45:29 +0300

    Update base.py

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -69,6 +69,13 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Auth
 
 AUTHENTICATION_BACKENDS = (
